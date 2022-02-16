f9ec4f75226c850a7bb4b53f8c7bfc6db60ce9b6
Krzysztof Strugiński <cagrin@gmail.com>
2022-02-17 00:11:51 +0100

    Add latest/Tests

---
+++ latest/test.ps1
@@ -0,0 +1,15 @@
+$Collation='Polish_CI_AS'
+$SA_PASSWORD='StrongP@ssw0rd!'
+$DB_NAME='tSQLt-edge'
+
+docker container stop test_image
+docker container rm test_image
+docker run --name test_image -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=$SA_PASSWORD" -e "MSSQL_COLLATION=$Collation" -p 51433:1433 -d mcr.microsoft.com/azure-sql-edge
+Start-Sleep -Second 30
+
+dotnet publish ./Tests /p:TargetServerName=localhost /p:TargetPort=51433 /p:TargetDatabaseName=$DB_NAME /p:TargetUser=sa /p:TargetPassword=$SA_PASSWORD
+
+Invoke-Sqlcmd -ServerInstance "localhost,51433" -Database "$DB_NAME" -Username "sa" -Password "$SA_PASSWORD" -Query "EXEC testSchema.test1" -Verbose
+
+docker container stop test_image
+docker container rm test_image
