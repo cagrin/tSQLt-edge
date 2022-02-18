d58f9178b98ce459ccbc29b126f665f504bc6bb3
Krzysztof Strugiński <cagrin@gmail.com>
2022-02-19 00:43:34 +0100

    Add "run internal command" approach

--- latest/Source/tSQLt/tSQLt.Fail.sql
+++ latest/Source/tSQLt/tSQLt.Fail.sql
@@ -11,6 +11,17 @@ CREATE PROCEDURE tSQLt.Fail
     @Message9 NVARCHAR(MAX) = ''
 AS
 BEGIN
-    PRINT '- tSQLt.Fail';
+    DECLARE @Command NVARCHAR(MAX) = 'tSQLt.Internal_Fail';
+    EXEC @Command
+    @Message0 = @Message0,
+    @Message1 = @Message1,
+    @Message2 = @Message2,
+    @Message3 = @Message3,
+    @Message4 = @Message4,
+    @Message5 = @Message5,
+    @Message6 = @Message6,
+    @Message7 = @Message7,
+    @Message8 = @Message8,
+    @Message9 = @Message9;
 END;
 GO
