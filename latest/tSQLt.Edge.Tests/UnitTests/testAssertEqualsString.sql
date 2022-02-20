dfcd20ce07eca011aa89a4273026af0c9c982d24
Krzysztof Strugiński <cagrin@gmail.com>
2022-02-20 17:03:31 +0100

    Add "RunAll" implementation

---
+++ latest/tSQLt.Edge.Tests/UnitTests/testAssertEqualsString.sql
@@ -0,0 +1,9 @@
+CREATE SCHEMA testAssertEqualsString;
+GO
+
+CREATE PROCEDURE testAssertEqualsString.[test print]
+AS
+BEGIN
+    PRINT '- testAssertEqualsString.[test print]';
+END;
+GO
--- latest/tSQLt.Edge/Internal/tSQLt.Internal_RunAll.sql
+++ latest/tSQLt.Edge/Internal/tSQLt.Internal_RunAll.sql
@@ -1,6 +1,29 @@
 CREATE PROCEDURE tSQLt.Internal_RunAll
 AS
 BEGIN
-    PRINT '- tSQLt.RunAll';
+    DECLARE @Command NVARCHAR(MAX) = '
+    DECLARE @Command NVARCHAR(MAX);
+    SELECT @Command = STRING_AGG(command, CHAR(13))
+    FROM
+    (
+        SELECT command = ''EXEC '' + spname + '';''
+        FROM
+        (
+            SELECT TOP (100) PERCENT
+                spname = QUOTENAME(SCHEMA_NAME(r.schema_id)) + ''.'' + QUOTENAME(r.name)
+            FROM sys.procedures r
+            LEFT JOIN sys.parameters p
+            ON p.object_id = r.object_id
+            WHERE SCHEMA_NAME(r.schema_id) <> ''tSQLt''
+            AND r.name LIKE ''test%''
+            AND p.parameter_id IS NULL
+            ORDER BY r.name
+        ) A
+        GROUP BY spname
+    ) B
+
+    EXEC (@Command);
+    ';
+    EXEC (@Command);
 END;
 GO
