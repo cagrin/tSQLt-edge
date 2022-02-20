c7ba1e262290ca522e57ee8e3cacffe93a56fe50
Krzysztof Strugiński <cagrin@gmail.com>
2022-02-20 17:19:07 +0100

    Add latest/test.ps1 to onPushMain.yml

--- latest/test.ps1
+++ latest/test.ps1
@@ -1,12 +1,10 @@
-$SA_PASSWORD='StrongP@ssw0rd!'
+#docker container stop test_image
+#docker container rm test_image
 
-<#
+$SA_PASSWORD='A.794613'
 $Collation='Polish_CI_AS'
-docker container stop test_image
-docker container rm test_image
 docker run --name test_image -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=$SA_PASSWORD" -e "MSSQL_COLLATION=$Collation" -p 51433:1433 -d mcr.microsoft.com/azure-sql-edge
 Start-Sleep -Second 30
-#>
 
 $TEST_NAME='tSQLt.API.Tests'
 dotnet publish ./$TEST_NAME /p:TargetServerName=localhost /p:TargetPort=51433 /p:TargetDatabaseName=$TEST_NAME /p:TargetUser=sa /p:TargetPassword=$SA_PASSWORD
@@ -15,6 +13,3 @@ Invoke-Sqlcmd -ServerInstance "localhost,51433" -Database "$TEST_NAME" -Username
 $TEST_NAME='tSQLt.Edge.Tests'
 dotnet publish ./$TEST_NAME /p:TargetServerName=localhost /p:TargetPort=51433 /p:TargetDatabaseName=$TEST_NAME /p:TargetUser=sa /p:TargetPassword=$SA_PASSWORD
 Invoke-Sqlcmd -ServerInstance "localhost,51433" -Database "$TEST_NAME" -Username "sa" -Password "$SA_PASSWORD" -Query "EXEC tSQLt.RunAll" -Verbose
-
-#docker container stop test_image
-#docker container rm test_image
