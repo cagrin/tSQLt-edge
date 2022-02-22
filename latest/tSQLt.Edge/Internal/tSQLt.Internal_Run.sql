19e954d2e82798d617be86122c40fa37edd6dc1f
Krzysztof Strugiński <cagrin@gmail.com>
2022-02-23 00:09:45 +0100

    Introduction to Internal_Run

--- latest/tSQLt.Edge/Internal/tSQLt.Internal_Run.sql
+++ latest/tSQLt.Edge/Internal/tSQLt.Internal_Run.sql
@@ -2,6 +2,7 @@ CREATE PROCEDURE tSQLt.Internal_Run
     @TestName NVARCHAR(MAX) = NULL
 AS
 BEGIN
-    PRINT CONCAT_WS(' ', '- tSQLt.Run', @TestName);
+    PRINT @TestName;
+    EXEC @TestName;
 END;
 GO
