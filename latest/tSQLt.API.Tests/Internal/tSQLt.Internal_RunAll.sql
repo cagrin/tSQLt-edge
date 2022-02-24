c7ea12ed50516bbb3a83b687dfae285f4ec8cf79
Krzysztof Strugiński <cagrin@gmail.com>
2022-02-25 00:29:19 +0100

    Add FORMATMESSAGE to API.Tests

--- latest/tSQLt.API.Tests/Internal/tSQLt.Internal_RunAll.sql
+++ latest/tSQLt.API.Tests/Internal/tSQLt.Internal_RunAll.sql
@@ -1,38 +1,32 @@
 CREATE PROCEDURE tSQLt.Internal_RunAll
 AS
 BEGIN
-    DECLARE @Command NVARCHAR(MAX) = '
     DECLARE @Command NVARCHAR(MAX);
-    SELECT @Command = STRING_AGG(CONVERT(NVARCHAR(MAX),''EXEC ('''''' + REPLACE(command, '''''''', '''''''''''') + '''''');''), CHAR(13)+ CHAR(10))
+    
+    SELECT @Command = STRING_AGG(CONVERT(NVARCHAR(MAX), FORMATMESSAGE('EXEC (''%s'');', REPLACE(command, '''', ''''''))), CHAR(13))
     FROM
     (
-
         SELECT
-        command = ''CREATE PROCEDURE '' + REPLACE(spname, ''.['', ''.[Internal_'') + ISNULL('' '' + STRING_AGG(name + '' '' + default_value, '', '') WITHIN GROUP (ORDER BY parameter_id), '''')
-            + '' AS BEGIN''
-            + '' PRINT CONCAT_WS('''' '''', ''''- '' + spname + '''''', '' + STRING_AGG(CONVERT(NVARCHAR(MAX),pname), '', '') + '');''
-            + '' END;''
+            command = FORMATMESSAGE
+            (
+                'CREATE PROCEDURE %s%s AS BEGIN PRINT ''%s, %s''; END;',
+                REPLACE(spname, '.[', '.[Internal_'),
+                STRING_AGG(pvalue, ',') WITHIN GROUP (ORDER BY parameter_id),
+                spname,
+                STRING_AGG(pname, ', ')
+            )
         FROM
         (
             SELECT TOP (100) PERCENT
-                spname = QUOTENAME(SCHEMA_NAME(r.schema_id)) + ''.'' + QUOTENAME(r.name),
-                p.name,
-                pname = CASE
-                    WHEN p.system_type_id <> 231  THEN '''''''' + p.name + ''''''''
-                    ELSE p.name
-                    END,
-                default_value = CASE
-                    WHEN p.system_type_id = 231 THEN ''NVARCHAR(MAX)''
-                    WHEN p.system_type_id = 104 THEN ''BIT''
-                    WHEN p.system_type_id = 98 THEN ''SQL_VARIANT''
-                    WHEN p.system_type_id = 56 THEN ''INT''
-                    END,
+                spname = FORMATMESSAGE('%s.%s', QUOTENAME(SCHEMA_NAME(r.schema_id)), QUOTENAME(r.name)),
+                pname = p.name,
+                pvalue = CASE WHEN p.name IS NOT NULL THEN FORMATMESSAGE(' %s %s', p.name, CASE WHEN p.system_type_id = 231 THEN 'NVARCHAR(MAX)' ELSE UPPER(TYPE_NAME(p.system_type_id )) END) ELSE '' END,
                 p.parameter_id
             FROM sys.procedures r
             LEFT JOIN sys.parameters p
             ON p.object_id = r.object_id
-            WHERE SCHEMA_NAME(r.schema_id) = ''tSQLt''
-            AND r.name <> ''RunAll''
+            WHERE SCHEMA_NAME(r.schema_id) = 'tSQLt'
+            AND r.name <> 'RunAll'
             ORDER BY r.name, p.parameter_id
         ) A
         GROUP BY spname
@@ -40,30 +34,33 @@ BEGIN
 
     EXEC (@Command);
 
-    SELECT @Command = STRING_AGG(command, CHAR(13))
+    SELECT @Command = STRING_AGG(CONVERT(NVARCHAR(MAX), command), CHAR(13))
     FROM
     (
-        SELECT command = ''EXEC '' + spname + ISNULL('' '' + STRING_AGG(name + '' = '' + default_value, '', '') WITHIN GROUP (ORDER BY parameter_id), '''') + '';''
+        SELECT
+            command = FORMATMESSAGE
+            (
+                'EXEC %s%s;',
+                spname,
+                STRING_AGG(pvalue, ',') WITHIN GROUP (ORDER BY parameter_id)
+            )
         FROM
         (
             SELECT TOP (100) PERCENT
-                spname = QUOTENAME(SCHEMA_NAME(r.schema_id)) + ''.'' + QUOTENAME(r.name),
-                p.name,
-                default_value = CASE WHEN p.system_type_id = 231 THEN '''''''' + p.name + '''''''' ELSE ''NULL'' END,
+                spname = FORMATMESSAGE('%s.%s', QUOTENAME(SCHEMA_NAME(r.schema_id)), QUOTENAME(r.name)),
+                pvalue = CASE WHEN p.name IS NOT NULL THEN FORMATMESSAGE(' %s = NULL', p.name) ELSE '' END,
                 p.parameter_id
             FROM sys.procedures r
             LEFT JOIN sys.parameters p
             ON p.object_id = r.object_id
-            WHERE SCHEMA_NAME(r.schema_id) = ''tSQLt''
-            AND r.name NOT LIKE ''Internal%''
-            AND r.name <> ''RunAll''
+            WHERE SCHEMA_NAME(r.schema_id) = 'tSQLt'
+            AND r.name NOT LIKE 'Internal%'
+            AND r.name <> 'RunAll'
             ORDER BY r.name, p.parameter_id
         ) A
         GROUP BY spname
     ) B
 
-    EXEC (@Command);
-    ';
     EXEC (@Command);
 END;
 GO
