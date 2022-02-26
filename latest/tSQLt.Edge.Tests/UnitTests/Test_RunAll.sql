45d6a0360091c8d604afae17d2af6909aaa97ef6
Krzysztof Strugiński <cagrin@gmail.com>
2022-02-26 21:46:50 +0100

    Add Test_Interfaces

--- latest/tSQLt.Edge.Tests/UnitTests/Test_RunAll.sql
+++ latest/tSQLt.Edge.Tests/UnitTests/Test_RunAll.sql
@@ -1,43 +1,12 @@
 CREATE SCHEMA Test_RunAll;
 GO
 
-CREATE PROCEDURE Test_RunAll.Test_Internal
+CREATE PROCEDURE Test_RunAll.Test_Interfaces
 AS
 BEGIN
-    DECLARE @Command NVARCHAR(MAX);
-/*
-    SELECT @Command = STRING_AGG(CONVERT(NVARCHAR(MAX), FORMATMESSAGE('EXEC (''%s'');', REPLACE(command, '''', ''''''))), CHAR(13))
-    FROM
-    (
-        SELECT
-            command = FORMATMESSAGE
-            (
-                'CREATE PROCEDURE %s%s AS BEGIN PRINT ''%s, %s''; END;',
-                REPLACE(spname, '.[', '.[Internal_'),
-                STRING_AGG(pvalue, ',') WITHIN GROUP (ORDER BY parameter_id),
-                spname,
-                STRING_AGG(pname, ', ')
-            )
-        FROM
-        (
-            SELECT TOP (100) PERCENT
-                spname = FORMATMESSAGE('%s.%s', QUOTENAME(SCHEMA_NAME(r.schema_id)), QUOTENAME(r.name)),
-                pname = p.name,
-                pvalue = CASE WHEN p.name IS NOT NULL THEN FORMATMESSAGE(' %s %s', p.name, CASE WHEN p.system_type_id = 231 THEN 'NVARCHAR(MAX)' ELSE UPPER(TYPE_NAME(p.system_type_id )) END) ELSE '' END,
-                p.parameter_id
-            FROM sys.procedures r
-            LEFT JOIN sys.parameters p
-            ON p.object_id = r.object_id
-            WHERE SCHEMA_NAME(r.schema_id) = 'tSQLt'
-            AND r.name <> 'RunAll'
-            ORDER BY r.name, p.parameter_id
-        ) A
-        GROUP BY spname
-    ) B
+    DECLARE @Actual NVARCHAR(MAX);
 
-    EXEC (@Command);
-
-    SELECT @Command = STRING_AGG(CONVERT(NVARCHAR(MAX), command), CHAR(13))
+    SELECT @Actual = STRING_AGG(CONVERT(NVARCHAR(MAX), command),NCHAR(10))
     FROM
     (
         SELECT
@@ -51,20 +20,46 @@ BEGIN
         (
             SELECT TOP (100) PERCENT
                 spname = FORMATMESSAGE('%s.%s', QUOTENAME(SCHEMA_NAME(r.schema_id)), QUOTENAME(r.name)),
-                pvalue = CASE WHEN p.name IS NOT NULL THEN FORMATMESSAGE(' %s = NULL', p.name) ELSE '' END,
+                pvalue = CASE WHEN p.name IS NOT NULL THEN FORMATMESSAGE(' %s', p.name) ELSE '' END,
                 p.parameter_id
             FROM sys.procedures r
             LEFT JOIN sys.parameters p
             ON p.object_id = r.object_id
             WHERE SCHEMA_NAME(r.schema_id) = 'tSQLt'
-            AND r.name NOT LIKE 'Internal%'
+            AND r.name NOT LIKE 'Internal[_]%'
+            AND r.name NOT LIKE 'Private[_]%'
             AND r.name <> 'RunAll'
             ORDER BY r.name, p.parameter_id
         ) A
         GROUP BY spname
     ) B
 
-    EXEC (@Command);
-*/
+    DECLARE @Expected NVARCHAR(MAX) =
+'EXEC [tSQLt].[ApplyConstraint] @TableName, @ConstraintName, @SchemaName, @NoCascade;
+EXEC [tSQLt].[ApplyTrigger] @TableName, @TriggerName;
+EXEC [tSQLt].[AssertEmptyTable] @TableName, @Message;
+EXEC [tSQLt].[AssertEquals] @Expected, @Actual, @Message;
+EXEC [tSQLt].[AssertEqualsString] @Expected, @Actual, @Message;
+EXEC [tSQLt].[AssertEqualsTable] @Expected, @Actual, @Message, @FailMsg;
+EXEC [tSQLt].[AssertEqualsTableSchema] @Expected, @Actual, @Message;
+EXEC [tSQLt].[AssertLike] @ExpectedPattern, @Actual, @Message;
+EXEC [tSQLt].[AssertNotEquals] @Expected, @Actual, @Message;
+EXEC [tSQLt].[AssertObjectDoesNotExist] @ObjectName, @Message;
+EXEC [tSQLt].[AssertObjectExists] @ObjectName, @Message;
+EXEC [tSQLt].[AssertResultSetsHaveSameMetaData] @expectedCommand, @actualCommand;
+EXEC [tSQLt].[DropClass] @ClassName;
+EXEC [tSQLt].[ExpectException] @ExpectedMessage, @ExpectedSeverity, @ExpectedState, @Message, @ExpectedMessagePattern, @ExpectedErrorNumber;
+EXEC [tSQLt].[ExpectNoException] @Message;
+EXEC [tSQLt].[Fail] @Message0, @Message1, @Message2, @Message3, @Message4, @Message5, @Message6, @Message7, @Message8, @Message9;
+EXEC [tSQLt].[FakeFunction] @FunctionName, @FakeFunctionName, @FakeDataSource;
+EXEC [tSQLt].[FakeTable] @TableName, @SchemaName, @Identity, @ComputedColumns, @Defaults;
+EXEC [tSQLt].[NewTestClass] @ClassName;
+EXEC [tSQLt].[RemoveObject] @ObjectName, @NewName, @IfExists;
+EXEC [tSQLt].[RemoveObjectIfExists] @ObjectName, @NewName;
+EXEC [tSQLt].[RenameClass] @SchemaName, @NewSchemaName;
+EXEC [tSQLt].[Run] @TestName;
+EXEC [tSQLt].[SpyProcedure] @ProcedureName, @CommandToExecute;';
+
+    EXEC tSQLt.AssertEqualsString @Expected, @Actual;
 END;
 GO
