99d904ee182a7a25e3bdfd6bb8437eb37305ee5e
Krzysztof Strugiński <cagrin@gmail.com>
2022-02-27 19:14:46 +0100

    Add trasaction to roll back test's modifications

--- latest/tSQLt.Edge/Run/tSQLt.Private_Run.sql
+++ latest/tSQLt.Edge/Run/tSQLt.Private_Run.sql
@@ -2,7 +2,35 @@ CREATE PROCEDURE tSQLt.Private_Run
     @TestName NVARCHAR(MAX) = NULL
 AS
 BEGIN
-    PRINT @TestName;
-    EXEC @TestName;
+    DECLARE @TranCounter INT;
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    DECLARE @ErrorSeverity INT;
+    DECLARE @ErrorState INT;
+
+    -- https://docs.microsoft.com/en-us/sql/t-sql/language-elements/save-transaction-transact-sql?view=sql-server-ver15
+    SET @TranCounter = @@TRANCOUNT;
+    IF @TranCounter = 0
+        BEGIN TRANSACTION;
+    ELSE
+        SAVE TRANSACTION TestName;
+
+    BEGIN TRY
+        PRINT @TestName;
+        EXEC @TestName;
+    END TRY
+    BEGIN CATCH
+        SELECT @ErrorMessage = ERROR_MESSAGE();
+        SELECT @ErrorSeverity = ERROR_SEVERITY();
+        SELECT @ErrorState = ERROR_STATE();
+    END CATCH
+
+    IF @TranCounter = 0
+        ROLLBACK TRANSACTION;
+    ELSE
+        IF XACT_STATE() <> -1
+            ROLLBACK TRANSACTION TestName;
+
+    IF @@ERROR <> 0
+        RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
 END;
 GO
