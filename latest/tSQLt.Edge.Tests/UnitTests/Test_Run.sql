03ce2551ed388ebbdddba86d64c58d97910ba64b
Krzysztof Strugiński <cagrin@gmail.com>
2022-02-28 16:54:27 +0100

    Simplifying Test_Run

--- latest/tSQLt.Edge.Tests/UnitTests/Test_Run.sql
+++ latest/tSQLt.Edge.Tests/UnitTests/Test_Run.sql
@@ -4,37 +4,23 @@ GO
 CREATE PROCEDURE Test_Run.Test_CreateNewTable
 AS
 BEGIN
-    BEGIN TRY
-        CREATE TABLE dbo.NewTable (Id INT);
+    CREATE TABLE dbo.NewTable (Id INT);
 
-        IF OBJECT_ID ('dbo.NewTable', 'U') IS NULL
-        BEGIN
-            EXEC tSQLt.Fail 'dbo.NewTable should exists.';
-        END
-    END TRY
-    BEGIN CATCH
-        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
-
-        EXEC tSQLt.Fail @ErrorMessage;
-    END CATCH
+    IF OBJECT_ID ('dbo.NewTable', 'U') IS NULL
+    BEGIN
+        EXEC tSQLt.Fail 'dbo.NewTable should exists.';
+    END
 END;
 GO
 
 CREATE PROCEDURE Test_Run.Test_NestedTransactions
 AS
 BEGIN
-    BEGIN TRY
-        EXEC tSQLt.Run '[Test_Run].[Test_CreateNewTable]';
+    EXEC tSQLt.Run '[Test_Run].[Test_CreateNewTable]';
 
-        IF OBJECT_ID ('dbo.NewTable', 'U') IS NOT NULL
-        BEGIN
-            EXEC tSQLt.Fail 'dbo.NewTable should not exists.';
-        END
-    END TRY
-    BEGIN CATCH
-        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
-
-        EXEC tSQLt.Fail @ErrorMessage;
-    END CATCH
+    IF OBJECT_ID ('dbo.NewTable', 'U') IS NOT NULL
+    BEGIN
+        EXEC tSQLt.Fail 'dbo.NewTable should not exists.';
+    END
 END;
 GO
