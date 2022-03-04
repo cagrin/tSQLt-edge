d8351564d587aeb88a28aba37fa5bf2b1722bbbc
Krzysztof Strugiński <cagrin@gmail.com>
2022-03-04 17:14:36 +0100

    Add Test_NewProcedureWithP1_CommandToExecute

--- latest/tSQLt.Edge.Tests/InterfaceTests/Test_SpyProcedure.sql
+++ latest/tSQLt.Edge.Tests/InterfaceTests/Test_SpyProcedure.sql
@@ -65,3 +65,22 @@ BEGIN
     END
 END;
 GO
+
+CREATE PROCEDURE Test_SpyProcedure.Test_NewProcedureWithP1_CommandToExecute
+AS
+BEGIN
+    EXEC ('CREATE PROCEDURE dbo.NewProcedure @P1 int OUTPUT AS BEGIN RETURN; END;');
+
+    EXEC tSQLt.SpyProcedure 'dbo.NewProcedure', 'SET @P1 = 2;';
+
+    DECLARE @P1 int = 1;
+    EXEC dbo.NewProcedure @P1 = @P1 OUTPUT;
+
+    IF NOT EXISTS (SELECT 1 FROM dbo.NewProcedure_SpyProcedureLog WHERE _id_ = 1 AND P1 = 1)
+    BEGIN
+        EXEC tSQLt.Fail 'dbo.NewProcedure_SpyProcedureLog should exists.';
+    END
+
+    EXEC tSQLt.AssertEquals 2, @P1, '@P1 should equals 2.';
+END;
+GO
