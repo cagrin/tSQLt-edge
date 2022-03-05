358099f4cb4486eaa4267984662988fa03a1563c
Krzysztof Strugiński <cagrin@gmail.com>
2022-03-05 16:05:13 +0100

    Add Test_Raiserror_Message9

--- latest/tSQLt.Edge.Tests/InterfaceTests/Test_Fail.sql
+++ latest/tSQLt.Edge.Tests/InterfaceTests/Test_Fail.sql
@@ -14,3 +14,17 @@ BEGIN
     END CATCH
 END;
 GO
+
+CREATE PROCEDURE Test_Fail.Test_Raiserror_Message9
+AS
+BEGIN
+    DECLARE @Expected NVARCHAR(MAX), @Actual NVARCHAR(MAX);
+    BEGIN TRY
+        EXEC tSQLt.Fail 'Test_Raiserror', 'M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', @Message9 = 'M9';
+    END TRY
+    BEGIN CATCH
+        SELECT @Expected = 'Test_Raiserror M1 M2 M3 M4 M5 M6 M7 M8 M9', @Actual = ERROR_MESSAGE();
+        EXEC tSQLt.AssertEqualsString @Expected, @Actual;
+    END CATCH
+END;
+GO
