dd939726e12dc8710a4133b0a6c374b328708881
Krzysztof Strugiński <cagrin@gmail.com>
2022-03-22 16:26:02 +0100

    Add Test_AssertResultSetsHaveSameMetaData

---
+++ latest/tSQLt.Edge.Tests/InterfaceTests/Test_AssertResultSetsHaveSameMetaData.sql
@@ -0,0 +1,57 @@
+CREATE SCHEMA Test_AssertResultSetsHaveSameMetaData;
+GO
+
+CREATE PROCEDURE Test_AssertResultSetsHaveSameMetaData.Test_Select1
+AS
+BEGIN
+    EXEC tSQLt.AssertResultSetsHaveSameMetaData 'SELECT 1 Column1', 'SELECT 1 Column1';
+END;
+GO
+
+CREATE PROC Test_AssertResultSetsHaveSameMetaData.Test_SelectNullCommand
+AS
+BEGIN
+    EXEC tSQLt.ExpectException 'Incorrect syntax near '')''.';
+
+    EXEC tSQLt.AssertResultSetsHaveSameMetaData 'SELECT 1 Column1', NULL;
+END;
+GO
+
+CREATE PROCEDURE Test_AssertResultSetsHaveSameMetaData.Test_SelectMissingTemp
+AS
+BEGIN
+    EXEC tSQLt.ExpectException 'Invalid object name ''#TestTable2''.';
+
+    EXEC tSQLt.AssertResultSetsHaveSameMetaData 'SELECT 1 Column1', 'SELECT * FROM #TestTable2';
+END;
+GO
+
+CREATE PROC Test_AssertResultSetsHaveSameMetaData.Test_SelectDivideBy0
+AS
+BEGIN
+    EXEC tSQLt.ExpectException 'Divide by zero error encountered.';
+
+    EXEC tSQLt.AssertResultSetsHaveSameMetaData 'SELECT 1 Column1', 'SELECT 1/0 Column1';
+END;
+GO
+
+CREATE PROC Test_AssertResultSetsHaveSameMetaData.Test_SelectIntBigint
+AS
+BEGIN
+    EXEC tSQLt.ExpectException 'tSQLt.AssertResultSetsHaveSameMetaData failed. Expected:<[Column1] int NULL> has different metadata than Actual:<[Column1] bigint NULL>.';
+
+    EXEC tSQLt.AssertResultSetsHaveSameMetaData 'SELECT CAST(1 AS INT) Column1', 'SELECT CAST(1 AS BIGINT) Column1';
+END;
+GO
+
+CREATE PROCEDURE Test_AssertResultSetsHaveSameMetaData.Test_SelectFromTemp
+AS
+BEGIN
+    CREATE TABLE #TestTable1 (Column1 INT, Column2 CHAR(3));
+    CREATE TABLE #TestTable2 (Column1 INT, Column2 CHAR(3));
+    INSERT INTO #TestTable1 VALUES (1, 'ABC');
+    INSERT INTO #TestTable2 VALUES (2, 'XYZ');
+
+    EXEC tSQLt.AssertResultSetsHaveSameMetaData 'SELECT * FROM #TestTable1', 'SELECT * FROM #TestTable2';
+END;
+GO
--- latest/tSQLt.Edge/Internal/tSQLt.Internal_AssertResultSetsHaveSameMetaData.sql
+++ latest/tSQLt.Edge/Internal/tSQLt.Internal_AssertResultSetsHaveSameMetaData.sql
@@ -1,8 +1,27 @@
 CREATE PROCEDURE tSQLt.Internal_AssertResultSetsHaveSameMetaData
-    @expectedCommand NVARCHAR(MAX),
-    @actualCommand NVARCHAR(MAX)
+    @ExpectedCommand NVARCHAR(MAX),
+    @ActualCommand NVARCHAR(MAX)
 AS
 BEGIN
-    PRINT CONCAT_WS(' ', '- tSQLt.AssertResultSetsHaveSameMetaData', @expectedCommand, @actualCommand);
+    DECLARE @ExpectedMetaData NVARCHAR(MAX); EXEC tSQLt.Private_GetMetaData @ExpectedCommand, @ExpectedMetaData OUTPUT;
+
+    DECLARE @ActualMetaData NVARCHAR(MAX); EXEC tSQLt.Private_GetMetaData @ActualCommand, @ActualMetaData OUTPUT;
+
+    IF (@ExpectedMetaData = @ActualMetaData)
+    BEGIN
+        RETURN;
+    END
+    ELSE
+    BEGIN
+        DECLARE @Failed NVARCHAR(MAX) = CONCAT
+        (
+            'tSQLt.AssertResultSetsHaveSameMetaData failed. Expected:<',
+            @ExpectedMetaData,
+            '> has different metadata than Actual:<',
+            @ActualMetaData,
+            '>.'
+        );
+        EXEC tSQLt.Fail @Message0 = @Failed;
+    END
 END;
 GO
