67bd7a5a6332fed3a317d87753adadd45e1b359a
Krzysztof Strugiński <cagrin@gmail.com>
2022-06-05 23:35:43 +0200

    Fix ExpectNoException #152

--- latest/tSQLt.Edge.Tests/InterfaceTests/Test_ExpectNoException.sql
+++ latest/tSQLt.Edge.Tests/InterfaceTests/Test_ExpectNoException.sql
@@ -32,3 +32,44 @@ BEGIN
         @ExpectedMessage = 'Message. Expected no exception to be raised. ErrorMessage:<Divide by zero error encountered.>. ErrorSeverity:<16>. ErrorState:<1>. ErrorNumber:<8134>.';
 END;
 GO
+
+CREATE PROCEDURE Test_ExpectNoException.Test_GoodAndFailSelect
+AS
+BEGIN
+    EXEC tSQLt.ExpectNoException;
+    SELECT 1.0 A INTO #Good;
+
+    EXEC tSQLt.ExpectException @ExpectedMessage = 'Divide by zero error encountered.';
+    SELECT 1/0 A INTO #Fail;
+END;
+GO
+
+CREATE PROCEDURE Test_ExpectNoException.Test_GoodAndFailSelectButFirstFail
+AS
+BEGIN
+    EXEC Test_Extensions.AssertCommandFails
+        @Command = '
+            EXEC tSQLt.ExpectNoException;
+            SELECT 1/0 A INTO #Good;
+
+            EXEC tSQLt.ExpectException @ExpectedMessage = ''Divide by zero error encountered.'';
+            SELECT 1/0 A INTO #Fail;
+        ',
+        @ExpectedMessage = 'Expected no exception to be raised. ErrorMessage:<Divide by zero error encountered.>. ErrorSeverity:<16>. ErrorState:<1>. ErrorNumber:<8134>.';
+END;
+GO
+
+CREATE PROCEDURE Test_ExpectNoException.Test_GoodAndFailSelectButSecondFail
+AS
+BEGIN
+    EXEC Test_Extensions.AssertCommandFails
+        @Command = '
+            EXEC tSQLt.ExpectNoException;
+            SELECT 1.0 A INTO #Good;
+
+            EXEC tSQLt.ExpectException @ExpectedMessage = ''Divide by zero error encountered.'';
+            SELECT 1.0 A INTO #Fail;
+        ',
+        @ExpectedMessage = 'Expected an exception to be raised. ExpectedMessage:<Divide by zero error encountered.>.';
+END;
+GO
