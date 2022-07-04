2f87da6a29f74322fd832aedde254f6a95953d70
Krzysztof Strugiński <cagrin@gmail.com>
2022-07-05 00:42:56 +0200

    Add 2 new ApplyIndex tests

--- latest/tSQLt.Edge.Tests/InterfaceTests/Test_ApplyIndex.sql
+++ latest/tSQLt.Edge.Tests/InterfaceTests/Test_ApplyIndex.sql
@@ -14,3 +14,37 @@ BEGIN
     EXEC tSQLt.ApplyIndex 'Schema1.Table1', 'Index1';
 END;
 GO
+
+CREATE PROCEDURE Test_ApplyIndex.Test_FilteredUniqueIndexApplied
+AS
+BEGIN
+    EXEC('CREATE SCHEMA Schema1;');
+    CREATE TABLE Schema1.Table1 (Column1 INT, Column2 CHAR(1));
+    CREATE UNIQUE INDEX FilteredUniqueIndex1 ON Schema1.Table1 (Column1) WHERE Column2 = 'Y'
+
+    EXEC tSQLt.FakeTable 'Schema1.Table1';
+    EXEC tSQLt.ApplyIndex 'Schema1.Table1', 'FilteredUniqueIndex1';
+
+    INSERT INTO Schema1.Table1 (Column1, Column2) VALUES (1, 'Y'), (1, 'N'), (2, 'Y')
+
+    EXEC tSQLt.ExpectException 'Cannot insert duplicate key row in object ''Schema1.Table1'' with unique index ''FilteredUniqueIndex1''. The duplicate key value is (1).'
+
+    INSERT INTO Schema1.Table1 (Column1, Column2) VALUES (1, 'Y')
+END;
+GO
+
+CREATE PROCEDURE Test_ApplyIndex.Test_ClusteredIndexApplied
+AS
+BEGIN
+    EXEC('CREATE SCHEMA Schema1;');
+    CREATE TABLE Schema1.Table1 (Column1 INT NOT NULL);
+    CREATE CLUSTERED INDEX ClusteredIndex1 ON Schema1.Table1 (Column1);
+
+    EXEC tSQLt.FakeTable 'Schema1.Table1';
+    EXEC tSQLt.ApplyIndex 'Schema1.Table1', 'ClusteredIndex1';
+
+    EXEC tSQLt.ExpectException 'Cannot create more than one clustered index on table ''Schema1.Table1''. Drop the existing clustered index ''ClusteredIndex1'' before creating another.';
+
+    CREATE CLUSTERED INDEX ClusteredIndex2 ON Schema1.Table1 (Column1);
+END;
+GO
--- latest/tSQLt.Edge/Internal/tSQLt.Internal_ApplyIndex.sql
+++ latest/tSQLt.Edge/Internal/tSQLt.Internal_ApplyIndex.sql
@@ -3,12 +3,52 @@ CREATE PROCEDURE tSQLt.Internal_ApplyIndex
     @IndexName NVARCHAR(MAX)
 AS
 BEGIN
-    DECLARE @Failed NVARCHAR(MAX) = CONCAT_WS
-    (
-        ' ',
-        'tSQLt.ApplyIndex failed.',
-        CONCAT('Index:<', @IndexName, '> on table <', @TableName,'> does not exist.')
-    );
-    EXEC tSQLt.Fail @Failed;
+    DECLARE @ObjectName NVARCHAR(MAX) = CONCAT(QUOTENAME(OBJECT_SCHEMA_NAME(OBJECT_ID(@TableName))), '.', QUOTENAME(OBJECT_NAME(OBJECT_ID(@TableName))));
+
+    EXEC tSQLt.AssertObjectExists @ObjectName;
+
+    DECLARE @ObjectId INT, @ParentName NVARCHAR(MAX);
+    SELECT
+        @ObjectId = [object_id],
+        @IndexName = QUOTENAME([index_name]),
+        @ParentName = CONCAT(QUOTENAME(SCHEMA_NAME([schema_id])), '.', QUOTENAME([table_name]))
+    FROM tSQLt.System_IndexColumns()
+    WHERE [schema_id] = SCHEMA_ID(OBJECT_SCHEMA_NAME(OBJECT_ID(@ObjectName))) --todo
+    AND ([index_name] = @IndexName OR QUOTENAME([index_name]) = @IndexName)
+
+    IF @ObjectId IS NOT NULL
+    BEGIN
+        DECLARE @IndexUnique NVARCHAR(MAX), @IndexType NVARCHAR(MAX), @IndexDefinition NVARCHAR(MAX);
+        SELECT
+            @IndexUnique = CASE WHEN [is_unique] = 1 THEN 'UNIQUE' END,
+            @IndexType = [type_desc],
+            @IndexDefinition = CONCAT
+            (
+                '(', STRING_AGG(QUOTENAME([column_name]), ', '), ')',
+                CASE WHEN [has_filter] = 1 THEN CONCAT_WS(' ', 'WHERE', [filter_definition]) END
+            )
+        FROM tSQLt.System_IndexColumns()
+        WHERE [object_id] = @ObjectId
+        GROUP BY [type_desc], [is_unique], [has_filter], [filter_definition]
+
+        DECLARE @CreateIndex NVARCHAR(MAX) = CONCAT_WS
+        (
+            ' ',
+            'DROP INDEX', @IndexName, 'ON', @ParentName,
+            'CREATE', @IndexUnique, @IndexType, 'INDEX', @IndexName, 'ON', @ObjectName, @IndexDefinition
+        )
+
+        EXEC (@CreateIndex);
+    END
+    ELSE
+    BEGIN
+        DECLARE @Failed NVARCHAR(MAX) = CONCAT_WS
+        (
+            ' ',
+            'tSQLt.ApplyIndex failed.',
+            CONCAT('Index:<', @IndexName, '> on table <', @TableName,'> does not exist.')
+        );
+        EXEC tSQLt.Fail @Failed;
+    END
 END;
 GO
