e3b06848e847b2ddcd6fdb1fe32271836ee87a71
Krzysztof Strugiński <cagrin@gmail.com>
2022-08-13 01:05:50 +0200

    Add Private_GetParametersWithTypes as procedure

--- latest/tSQLt.Edge/Private/tSQLt.Private_GetParametersWithTypes.sql
+++ latest/tSQLt.Edge/Private/tSQLt.Private_GetParametersWithTypes.sql
@@ -7,6 +7,68 @@ BEGIN
     INSERT INTO @System_Parameters
     EXEC tSQLt.System_Parameters @ObjectId
 
+    DECLARE @Result TABLE
+    (
+        [_id_] INT IDENTITY(1,1),
+        [definition] NVARCHAR(MAX),
+        [name] [sysname],
+        [parameter_id] [int],
+        [user_type_id] [int],
+        [max_length] [smallint],
+        [precision] [tinyint],
+        [scale] [tinyint],
+        [is_output] [bit]
+    )
+
+    INSERT INTO @Result
+    (
+        [name],
+        [parameter_id],
+        [user_type_id],
+        [max_length],
+        [precision],
+        [scale],
+        [is_output]
+    )
+    SELECT
+        [name],
+        [parameter_id],
+        [user_type_id],
+        [max_length],
+        [precision],
+        [scale],
+        [is_output]
+    FROM @System_Parameters
+    ORDER BY parameter_id
+
+    DECLARE
+        @_i_ INT = 1,
+        @_m_ INT = (SELECT COUNT(1) FROM @Result),
+        @user_type_id [int],
+        @max_length [smallint],
+        @precision [tinyint],
+        @scale [tinyint]
+
+    WHILE (@_i_ <= @_m_)
+    BEGIN
+        SELECT
+            @user_type_id = [user_type_id],
+            @max_length = [max_length],
+            @precision = [precision],
+            @scale = [scale]
+        FROM @Result
+        WHERE _id_ = @_i_
+
+        DECLARE @Type NVARCHAR(MAX)
+        EXEC tSQLt.Private_GetType @Type OUTPUT, @user_type_id, @max_length, @precision, @scale
+
+        UPDATE @Result
+        SET definition = @Type
+        WHERE _id_ = @_i_
+
+        SET @_i_ = @_i_ + 1
+    END
+
     SELECT
         @ParametersWithTypes = STRING_AGG
         (
@@ -14,11 +76,11 @@ BEGIN
             (
                 ' ',
                 name,
-                tSQLt.Private_GetType2(user_type_id, max_length, precision, scale, NULL),
+                definition,
                 CASE WHEN is_output = 1 THEN 'OUTPUT' ELSE NULL END
             ),
             ', '
         ) WITHIN GROUP (ORDER BY parameter_id)
-    FROM @System_Parameters
+    FROM @Result
 END;
 GO
