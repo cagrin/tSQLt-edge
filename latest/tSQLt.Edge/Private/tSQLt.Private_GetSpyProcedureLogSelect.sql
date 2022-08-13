cd58d70ccc6656405dc862683e828e0c46d180c4
Krzysztof Strugiński <cagrin@gmail.com>
2022-08-14 00:20:34 +0200

    Add Private_GetSpyProcedureLogSelect as procedure

--- latest/tSQLt.Edge/Private/tSQLt.Private_GetSpyProcedureLogSelect.sql
+++ latest/tSQLt.Edge/Private/tSQLt.Private_GetSpyProcedureLogSelect.sql
@@ -7,6 +7,57 @@ BEGIN
     INSERT INTO @System_Parameters
     EXEC tSQLt.System_Parameters @ObjectId
 
+    DECLARE @Result TABLE
+    (
+        [_id_] INT IDENTITY(1,1),
+        [definition] NVARCHAR(MAX),
+        [is_table_type] BIT,
+        [name] [sysname] NULL,
+        [parameter_id] [int] NOT NULL,
+        [user_type_id] [int] NOT NULL,
+        [is_output] [bit] NOT NULL
+    )
+
+    INSERT INTO @Result
+    (
+        [name],
+        [parameter_id],
+        [user_type_id],
+        [is_output]
+    )
+    SELECT
+        [name],
+        [parameter_id],
+        [user_type_id],
+        [is_output]
+    FROM @System_Parameters
+    ORDER BY parameter_id
+
+    DECLARE
+        @_i_ INT = 1,
+        @_m_ INT = (SELECT COUNT(1) FROM @Result),
+        @user_type_id [int]
+
+    WHILE (@_i_ <= @_m_)
+    BEGIN
+        SELECT
+            @user_type_id = [user_type_id]
+        FROM @Result
+        WHERE _id_ = @_i_
+
+        DECLARE @IsTableType NVARCHAR(MAX)
+        DECLARE @Types tSQLt.System_TypesType
+        INSERT INTO @Types
+        EXEC tSQLt.System_Types @user_type_id
+        SELECT @IsTableType = is_table_type FROM @Types
+
+        UPDATE @Result
+        SET is_table_type = @IsTableType
+        WHERE _id_ = @_i_
+
+        SET @_i_ = @_i_ + 1
+    END
+
     SELECT
         @SpyProcedureLogSelect = STRING_AGG
         (
@@ -21,10 +72,6 @@ BEGIN
             ),
             ', '
         ) WITHIN GROUP (ORDER BY parameter_id)
-    FROM
-    (
-        SELECT *, is_table_type = (SELECT is_table_type FROM tSQLt.System_Types2(user_type_id))
-        FROM @System_Parameters
-    ) P
+    FROM @Result
 END;
 GO
