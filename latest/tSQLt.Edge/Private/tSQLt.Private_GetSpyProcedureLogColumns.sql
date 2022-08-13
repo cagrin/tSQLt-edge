990a9fab3909b6b125b088fb274e32403d645f1c
Krzysztof Strugiński <cagrin@gmail.com>
2022-08-14 00:14:45 +0200

    Add Private_GetSpyProcedureLogColumns as procedure

--- latest/tSQLt.Edge/Private/tSQLt.Private_GetSpyProcedureLogColumns.sql
+++ latest/tSQLt.Edge/Private/tSQLt.Private_GetSpyProcedureLogColumns.sql
@@ -7,6 +7,74 @@ BEGIN
     INSERT INTO @System_Parameters
     EXEC tSQLt.System_Parameters @ObjectId
 
+    DECLARE @Result TABLE
+    (
+        [_id_] INT IDENTITY(1,1),
+        [definition] NVARCHAR(MAX),
+        [is_table_type] BIT,
+        [name] [sysname] NULL,
+        [parameter_id] [int] NOT NULL,
+        [user_type_id] [int] NOT NULL,
+        [max_length] [smallint] NOT NULL,
+        [precision] [tinyint] NOT NULL,
+        [scale] [tinyint] NOT NULL
+    )
+
+    INSERT INTO @Result
+    (
+        [name],
+        [parameter_id],
+        [user_type_id],
+        [max_length],
+        [precision],
+        [scale]
+    )
+    SELECT
+        [name],
+        [parameter_id],
+        [user_type_id],
+        [max_length],
+        [precision],
+        [scale]
+    FROM @System_Parameters
+    ORDER BY parameter_id
+
+    DECLARE
+        @_i_ INT = 1,
+        @_m_ INT = (SELECT COUNT(1) FROM @Result),
+        @user_type_id [int],
+        @max_length [smallint],
+        @precision [tinyint],
+        @scale [tinyint]
+
+    WHILE (@_i_ <= @_m_)
+    BEGIN
+        SELECT
+            @user_type_id = [user_type_id],
+            @max_length = [max_length],
+            @precision = [precision],
+            @scale = [scale]
+        FROM @Result
+        WHERE _id_ = @_i_
+
+        DECLARE @Type NVARCHAR(MAX)
+        EXEC tSQLt.Private_GetType @Type OUTPUT, @user_type_id, @max_length, @precision, @scale
+
+        DECLARE @IsTableType NVARCHAR(MAX)
+        DECLARE @Types tSQLt.System_TypesType
+        INSERT INTO @Types
+        EXEC tSQLt.System_Types @user_type_id
+        SELECT @IsTableType = is_table_type FROM @Types
+
+        UPDATE @Result
+        SET
+            definition = @Type,
+            is_table_type = @IsTableType
+        WHERE _id_ = @_i_
+
+        SET @_i_ = @_i_ + 1
+    END
+
     SELECT
         @SpyProcedureLogColumns = STRING_AGG
         (
@@ -16,16 +84,12 @@ BEGIN
                 REPLACE(name, '@', ''),
                 CASE
                     WHEN is_table_type = 1 THEN 'xml'
-                    ELSE tSQLt.Private_GetType2(user_type_id, max_length, precision, scale, NULL)
+                    ELSE definition
                 END,
                 'NULL'
             ),
             ', '
         ) WITHIN GROUP (ORDER BY parameter_id)
-    FROM
-    (
-        SELECT *, is_table_type = (SELECT is_table_type FROM tSQLt.System_Types2(user_type_id))
-        FROM @System_Parameters
-    ) P
+    FROM @Result
 END;
 GO
