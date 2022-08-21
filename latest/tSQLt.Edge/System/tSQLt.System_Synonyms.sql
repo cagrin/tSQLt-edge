0ba23ab490204f780cd281f1177b6f50fcbe4665
Krzysztof Strugiński <cagrin@gmail.com>
2022-08-21 09:13:12 +0200

    Fix System_Synonyms

--- latest/tSQLt.Edge/System/tSQLt.System_Synonyms.sql
+++ latest/tSQLt.Edge/System/tSQLt.System_Synonyms.sql
@@ -3,7 +3,7 @@ CREATE TYPE tSQLt.System_SynonymsType AS TABLE
 	[name] [sysname] NOT NULL,
 	[object_id] [int] NOT NULL,
 	[principal_id] [int] NULL,
-	[schema_id] [int] NOT NULL,
+	[schema_name] [sysname] NOT NULL,
 	[parent_object_id] [int] NOT NULL,
 	[type] [char](2) NULL,
 	[type_desc] [nvarchar](60) NULL,
@@ -20,9 +20,35 @@ CREATE PROCEDURE tSQLt.System_Synonyms
 	@ObjectName NVARCHAR(MAX)
 AS
 BEGIN
-	EXEC tSQLt.System_Table
-		@SysTableType = 'System_SynonymsType',
-		@SysTableName = 'sys.synonyms',
-		@ObjectName = @ObjectName
+	DECLARE @Command NVARCHAR(MAX) =
+	'SELECT
+		[name],
+		[object_id],
+		[principal_id],
+		[schema_name] = SCHEMA_NAME(schema_id),
+		[parent_object_id],
+		[type],
+		[type_desc],
+		[create_date],
+		[modify_date],
+		[is_ms_shipped],
+		[is_published],
+		[is_schema_published],
+		[base_object_name]
+	FROM sys.synonyms
+	WHERE object_id = OBJECT_ID(@ObjectName)'
+
+	DECLARE @DatabaseName NVARCHAR(MAX) = QUOTENAME(PARSENAME(@ObjectName, 3))
+	IF @DatabaseName IS NOT NULL
+	BEGIN
+		SET @Command = CONCAT
+		(
+			'EXEC(''USE ', @DatabaseName, '; ',
+            'DECLARE @ObjectName NVARCHAR(MAX) = ''''', @ObjectName, '''''; ',
+			'EXEC sys.sp_executesql N''''', REPLACE(@Command, '''', ''''''''''), ''''', N''''@ObjectName NVARCHAR(MAX)'''', @ObjectName;'')'
+		)
+	END
+
+	EXEC sys.sp_executesql @Command, N'@ObjectName NVARCHAR(MAX)', @ObjectName;
 END;
 GO
