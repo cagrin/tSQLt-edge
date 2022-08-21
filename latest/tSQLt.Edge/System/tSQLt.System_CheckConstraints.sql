91acc99de2d09c09eb55dcb2484b4765a0812689
Krzysztof Strugiński <cagrin@gmail.com>
2022-08-21 09:25:57 +0200

    Fix System_CheckConstraintsType

--- latest/tSQLt.Edge/System/tSQLt.System_CheckConstraints.sql
+++ latest/tSQLt.Edge/System/tSQLt.System_CheckConstraints.sql
@@ -3,7 +3,7 @@ CREATE TYPE tSQLt.System_CheckConstraintsType AS TABLE
 	[name] [sysname] NOT NULL,
 	[object_id] [int] NOT NULL,
 	[principal_id] [int] NULL,
-	[schema_id] [int] NOT NULL,
+	[schema_name] [sysname] NOT NULL,
 	[parent_object_id] [int] NOT NULL,
 	[type] [char](2) NULL,
 	[type_desc] [nvarchar](60) NULL,
@@ -27,22 +27,40 @@ CREATE PROCEDURE tSQLt.System_CheckConstraints
 	@ConstraintId INT
 AS
 BEGIN
-	DECLARE @SourceTable NVARCHAR(MAX) = 'sys.check_constraints'
-	IF PARSENAME(@ObjectName, 3) IS NOT NULL
+	DECLARE @Command NVARCHAR(MAX) =
+	'SELECT
+		[name],
+		[object_id],
+		[principal_id],
+		[schema_name] = SCHEMA_NAME(schema_id),
+		[parent_object_id],
+		[type],
+		[type_desc],
+		[create_date],
+		[modify_date],
+		[is_ms_shipped],
+		[is_published],
+		[is_schema_published],
+		[is_disabled],
+		[is_not_for_replication],
+		[is_not_trusted],
+		[parent_column_id],
+		[definition],
+		[uses_database_collation],
+		[is_system_named]
+	FROM sys.check_constraints
+	WHERE object_id = @ConstraintId'
+
+	DECLARE @DatabaseName NVARCHAR(MAX) = QUOTENAME(PARSENAME(@ObjectName, 3))
+	IF @DatabaseName IS NOT NULL
 	BEGIN
-		SET @SourceTable = CONCAT(QUOTENAME(PARSENAME(@ObjectName, 3)), '.', @SourceTable)
+		SET @Command = CONCAT
+		(
+			'EXEC(''USE ', @DatabaseName, '; ',
+            'DECLARE @ConstraintId INT = ', @ConstraintId, '; ',
+			'EXEC sys.sp_executesql N''''', REPLACE(@Command, '''', ''''''''''), ''''', N''''@ConstraintId INT'''', @ConstraintId;'')'
+		)
 	END
-
-	DECLARE @TableTypeColumns NVARCHAR(MAX)
-	EXEC tSQLt.System_GetTableTypeColumns @TableTypeColumns OUTPUT, @TableTypeName = 'System_CheckConstraintsType'
-
-	DECLARE @Command NVARCHAR(MAX) = CONCAT_WS
-	(
-		' ',
-		'SELECT', @TableTypeColumns,
-		'FROM', @SourceTable,
-		'WHERE object_id = @ConstraintId'
-	);
 
 	EXEC sys.sp_executesql @Command, N'@ConstraintId INT', @ConstraintId;
 END;
