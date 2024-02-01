32004c3dc50ee0ce7f5fe0987d00402598423539
Krzysztof Strugiński <cagrin@gmail.com>
2024-02-01 12:58:45 +0100

    Add more legacy tSQLt unoriginal tests

--- legacy/tSQLt.Unoriginal.Tests/Debug/tSQLt.Internal_FailDebug.sql
+++ legacy/tSQLt.Unoriginal.Tests/Debug/tSQLt.Internal_FailDebug.sql
@@ -28,6 +28,42 @@ BEGIN
 
     DECLARE @NL NVARCHAR(MAX) = CHAR(13) + CHAR(10);
 
+    -- [Test_AssertEquals].[Test_5_6]
+    IF  @ErrorMessage = 'Expected: <5> but was: <6>'
+    SET @ErrorMessage = 'tSQLt.AssertEquals failed. Expected:<5>. Actual:<6>.';
+
+    -- [Test_AssertEquals].[Test_ErrorMessage]
+    IF  @ErrorMessage = 'Error message.  Expected: <hello> but was: <hallo>'
+    SET @ErrorMessage = 'Error message. tSQLt.AssertEquals failed. Expected:<hello>. Actual:<hallo>.';
+
+    -- [Test_AssertEquals].[Test_FloatLossOfPrecision]
+    IF  @ErrorMessage = 'Expected: <0.3> but was: <0.3>'
+    SET @ErrorMessage = 'tSQLt.AssertEquals failed. Expected:<0.3>. Actual:<0.3>.';
+
+    -- [Test_AssertEquals].[Test_Hello_Hallo]
+    IF  @ErrorMessage = 'Expected: <hello> but was: <hallo>'
+    SET @ErrorMessage = 'tSQLt.AssertEquals failed. Expected:<hello>. Actual:<hallo>.';
+
+    -- [Test_AssertEquals].[Test_Hello_NULL]
+    IF  @ErrorMessage = 'Expected: <hello> but was: <NULL>'
+    SET @ErrorMessage = 'tSQLt.AssertEquals failed. Expected:<hello>. Actual:<(null)>.';
+
+    -- [Test_AssertEquals].[Test_Pi_Pi1]
+    IF  @ErrorMessage = 'Expected: <3.14> but was: <3.141>'
+    SET @ErrorMessage = 'tSQLt.AssertEquals failed. Expected:<3.14>. Actual:<3.141>.';
+
+    -- [Test_AssertEqualsString].[Test_ErrorMessage]
+    IF  @ErrorMessage LIKE 'Error message. __Expected: <hello>__but was : <hallo>'
+    SET @ErrorMessage = 'Error message. tSQLt.AssertEqualsString failed. Expected:<hello>. Actual:<hallo>.';
+
+    -- [Test_AssertEqualsString].[Test_Hello_Hallo]
+    IF  @ErrorMessage LIKE '__Expected: <hello>__but was : <hallo>'
+    SET @ErrorMessage = 'tSQLt.AssertEqualsString failed. Expected:<hello>. Actual:<hallo>.';
+
+    -- [Test_AssertEqualsString].[Test_Hello_NULL]
+    IF  @ErrorMessage LIKE '__Expected: <hello>__but was : NULL'
+    SET @ErrorMessage = 'tSQLt.AssertEqualsString failed. Expected:<hello>. Actual:<(null)>.';
+
     -- [Test_AssertLike].[Test_NullH_llo]
     IF  @ErrorMessage LIKE '__Expected: <H[_]llo>__ but was: <NULL>'
     SET @ErrorMessage = 'tSQLt.AssertLike failed. ExpectedPattern:<H_llo> but Actual:<(null)>.';
@@ -43,6 +79,70 @@ BEGIN
     -- [Test_AssertLike].[Test_ErrorMessage]
     IF  @ErrorMessage LIKE 'Error message. __Expected: <NULL>__ but was: <Hello>'
     SET @ErrorMessage = 'Error message. tSQLt.AssertLike failed. ExpectedPattern:<(null)> but Actual:<Hello>.';
+
+    --- [Test_AssertNotEquals].[Test_NULL_NULL]
+    IF  @ErrorMessage = 'Expected actual value to not be NULL.'
+    SET @ErrorMessage = 'tSQLt.AssertNotEquals failed. Expected any value except:<(null)>.';
+
+    -- [Test_AssertNotEquals].[Test_Pi_Pi]
+    IF  @ErrorMessage = 'Expected actual value to not equal <3.14>.'
+    SET @ErrorMessage = 'tSQLt.AssertNotEquals failed. Expected any value except:<3.14>.';
+
+    -- [Test_AssertNotEquals].[Test_Hello_Hello]
+    IF  @ErrorMessage = 'Expected actual value to not equal <hello>.'
+    SET @ErrorMessage = 'tSQLt.AssertNotEquals failed. Expected any value except:<hello>.';
+
+    -- [Test_AssertNotEquals].[Test_5_5]
+    IF  @ErrorMessage = 'Expected actual value to not equal <5>.'
+    SET @ErrorMessage = 'tSQLt.AssertNotEquals failed. Expected any value except:<5>.';
+
+    -- [Test_AssertNotEquals].[Test_ErrorMessage]
+    IF  @ErrorMessage = 'Error message. Expected actual value to not equal <hello>.'
+    SET @ErrorMessage = 'Error message. tSQLt.AssertNotEquals failed. Expected any value except:<hello>.';
+
+    -- [Test_AssertObjectDoesNotExist].[Test_DoesExist]
+    IF  @ErrorMessage = '''dbo.TestTable1'' does exist!'
+    SET @ErrorMessage = 'tSQLt.AssertObjectDoesNotExist failed. Object:<dbo.TestTable1> does exist.';
+
+    -- [Test_AssertObjectDoesNotExist].[Test_ExternalDoesExist]
+    IF  @ErrorMessage = '''master.dbo.TestTable1'' does exist!'
+    SET @ErrorMessage = 'tSQLt.AssertObjectDoesNotExist failed. Object:<master.dbo.TestTable1> does exist.';
+
+    -- [Test_AssertObjectDoesNotExist].[Test_TempDoesExist]
+    IF  @ErrorMessage = '''#TestTable1'' does exist!'
+    SET @ErrorMessage = 'tSQLt.AssertObjectDoesNotExist failed. Object:<#TestTable1> does exist.';
+
+    -- [Test_AssertObjectDoesNotExist].[Test_ErrorMessage]
+    IF  @ErrorMessage = 'Error message. ''dbo.TestTable1'' does exist!'
+    SET @ErrorMessage = 'Error message. tSQLt.AssertObjectDoesNotExist failed. Object:<dbo.TestTable1> does exist.';
+
+    -- [AssertObjectExists].[Test_ExternalDoesNotExist]
+    IF  @ErrorMessage = '''master.dbo.TestTable1'' does not exist'
+    SET @ErrorMessage = 'tSQLt.AssertObjectExists failed. Object:<master.dbo.TestTable1> does not exist.';
+
+    -- [AssertObjectExists].[Test_ErrorMessage]
+    IF  @ErrorMessage = 'Error message. ''dbo.TestTable1'' does not exist'
+    SET @ErrorMessage = 'Error message. tSQLt.AssertObjectExists failed. Object:<dbo.TestTable1> does not exist.';
+
+    -- [AssertObjectExists].[Test_DoesNotExist]
+    IF  @ErrorMessage = '''dbo.TestTable1'' does not exist'
+    SET @ErrorMessage = 'tSQLt.AssertObjectExists failed. Object:<dbo.TestTable1> does not exist.';
+
+    -- [AssertObjectExists].[Test_TempDoesNotExist]
+    IF  @ErrorMessage = '''#TestTable1'' does not exist'
+    SET @ErrorMessage = 'tSQLt.AssertObjectExists failed. Object:<#TestTable1> does not exist.';
+
+    -- [AssertStringIn].[Test_Empty_Hello]
+    IF  @ErrorMessage LIKE '<Hello>__is not in__|value|__+-----+'
+    SET @ErrorMessage = 'tSQLt.AssertStringIn failed. String:<Hello> is not in <(null)>.';
+
+    -- [AssertStringIn].[Test_Empty_Null]
+    IF  @ErrorMessage LIKE 'NULL__is not in__|value|__+-----+'
+    SET @ErrorMessage = 'tSQLt.AssertStringIn failed. String:<(null)> is not in <(null)>.';
+
+    -- [AssertStringIn].[Test_Hello_Hallo]
+    IF  @ErrorMessage LIKE '<Hallo>__is not in__|value|__+-----+__|Hello|__|World|'
+    SET @ErrorMessage = 'tSQLt.AssertStringIn failed. String:<Hallo> is not in <Hello, World>.';
 
     RAISERROR(N'%s', 16, 10, @ErrorMessage);
 END;
--- legacy/tSQLt.Unoriginal.Tests/Post-Deployment/Debug.sql
+++ legacy/tSQLt.Unoriginal.Tests/Post-Deployment/Debug.sql
@@ -36,20 +36,125 @@ END;
 ';
     EXEC (@AlterFail);
 
+    -- Setup
     EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_ApplyConstraint] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_ApplyTrigger] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertEmptyTable] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertEquals] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertEqualsString] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertEqualsTable] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertEqualsTableSchema] TO [tSQLt.TestClass];')
     EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertLike] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertNotEquals] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertObjectDoesNotExist] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertObjectExists] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertResultSetsHaveSameMetaData] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_AssertStringIn] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_ExpectException] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_ExpectNoException] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_Fail] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_FakeFunction] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_FakeTable] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_RemoveObject] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_RemoveObjectIfExists] TO [tSQLt.TestClass];')
+    EXEC ('ALTER AUTHORIZATION ON SCHEMA::[Test_SpyProcedure] TO [tSQLt.TestClass];')
 
+    -- Failure
     EXEC ('DROP PROCEDURE [Test_ApplyConstraint].[Test_ConstraintExistsOnOtherTable]');
     EXEC ('DROP PROCEDURE [Test_ApplyConstraint].[Test_ConstraintNotExists]');
     EXEC ('DROP PROCEDURE [Test_ApplyConstraint].[Test_PrimaryKeyApplied_WithDescendingKey]');
     EXEC ('DROP PROCEDURE [Test_ApplyConstraint].[Test_TableNotExists]');
     EXEC ('DROP PROCEDURE [Test_ApplyConstraint].[Test_TableSchemaNotExists]');
     EXEC ('DROP PROCEDURE [Test_ApplyConstraint].[Test_TableWasNotFaked]');
+    EXEC ('DROP PROCEDURE [Test_ApplyTrigger].[Test_FailWhenTableWasNotFaked]');
+    EXEC ('DROP PROCEDURE [Test_ApplyTrigger].[Test_TableNotExists]');
+    EXEC ('DROP PROCEDURE [Test_ApplyTrigger].[Test_TriggerExistsOnOtherTable]');
+    EXEC ('DROP PROCEDURE [Test_ApplyTrigger].[Test_TriggerNotExists]');
+    EXEC ('DROP PROCEDURE [Test_AssertEmptyTable].[Test_ErrorMessage]');
+    EXEC ('DROP PROCEDURE [Test_AssertEmptyTable].[Test_NonEmptyExternalTable]');
+    EXEC ('DROP PROCEDURE [Test_AssertEmptyTable].[Test_NonEmptyTable]');
+    EXEC ('DROP PROCEDURE [Test_AssertEmptyTable].[Test_NonEmptyTempTable]');
+    EXEC ('DROP PROCEDURE [Test_AssertEmptyTable].[Test_NonExistsTable]');
+    EXEC ('DROP PROCEDURE [Test_AssertEmptyTable].[Test_NonExistsTempTable]');
+    EXEC ('DROP PROCEDURE [Test_AssertEmptyTable].[Test_NullCommand]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_ErrorMessage]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_FirstTableHasTwinRows]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_FirstTableIsNotEmpty]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_FloatLossOfPrecision]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_LowDate]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_SecondTableHasTwinRows]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_SecondTableIsNotEmpty]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_TempTables_ActualNotExists]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_TempTables_ExpectedNotExists]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_UncomparableDataTypes]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTableSchema].[Test_DifferentColumnCollation]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTableSchema].[Test_DifferentColumnNames]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTableSchema].[Test_DifferentColumnNullable]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTableSchema].[Test_ErrorMessage]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTableSchema].[Test_TempTables_ActualNotExists]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTableSchema].[Test_TempTables_ExpectedNotExists]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTableSchema].[Test_TempTablesFail]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTableSchema].[Test_TempTablesOptionsFail]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTableSchema].[Test_UserTypeWithDifferentSchema]');
+    EXEC ('DROP PROCEDURE [Test_AssertResultSetsHaveSameMetaData].[Test_ActualCommandIsNull]');
+    EXEC ('DROP PROCEDURE [Test_AssertResultSetsHaveSameMetaData].[Test_ExpectedCommandIsNull]');
+    EXEC ('DROP PROCEDURE [Test_AssertResultSetsHaveSameMetaData].[Test_SelectDivideBy0]');
+    EXEC ('DROP PROCEDURE [Test_AssertResultSetsHaveSameMetaData].[Test_SelectIntBigint]');
+    EXEC ('DROP PROCEDURE [Test_AssertResultSetsHaveSameMetaData].[Test_SelectMissingTemp]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_ComputedColumnWithScalarFunctionFailed]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_Failed_When_Procedure]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_ScalarFunction_FakeFunctionIsNull]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_ScalarFunction_FunctionIsNull]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_SchemaAndTableNameNotExists]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_SchemaAndTableReversedNameNotExists]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_TableNameIsNull]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_TableNameNotExists]');
+    EXEC ('DROP PROCEDURE [Test_FakeTable].[Test_CanFakeTempTable]');
+    EXEC ('DROP PROCEDURE [Test_FakeTable].[Test_CannotFakeSynonymForProcedure]');
+    EXEC ('DROP PROCEDURE [Test_FakeTable].[Test_CannotFakeTableAndViewWithSchemabinding]');
+    EXEC ('DROP PROCEDURE [Test_RemoveObject].[Test_Table_NotExists]');
+    EXEC ('DROP PROCEDURE [Test_RemoveObject].[Test_TempTable]');
+    EXEC ('DROP PROCEDURE [Test_SpyProcedure].[Test_ProcedureDoesNotExist]');
+
+    -- Error
     EXEC ('DROP PROCEDURE [Test_ApplyConstraint].[Test_ExternalCheckConstraintApplied]');
     EXEC ('DROP PROCEDURE [Test_ApplyConstraint].[Test_ExternalForeignKeyApplied]');
     EXEC ('DROP PROCEDURE [Test_ApplyConstraint].[Test_ExternalPrimaryKeyApplied]');
     EXEC ('DROP PROCEDURE [Test_ApplyConstraint].[Test_ExternalUniqueConstraintApplied]');
-
+    EXEC ('DROP PROCEDURE [Test_ApplyTrigger].[Test_IsExternalTriggeredAfterFakeTableAndApplyTrigger]');
+    EXEC ('DROP PROCEDURE [Test_AssertEmptyTable].[Test_QuotedEmptyTempTable]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_XmlComparison]');
+    EXEC ('DROP PROCEDURE [Test_AssertEqualsTable].[Test_XmlComparisonToString]');
+    EXEC ('DROP PROCEDURE [Test_AssertStringIn].[Test_Null_Null]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_BadErrorMessage]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_BadErrorMessageWithMessage]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_BadExpectedErrorNumber]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_BadExpectedMessagePattern]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_BadExpectedSeverity]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_BadExpectedState]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_EmptyExec]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_GoodSelect]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_GoodSelectWithMessage]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_GoodSelectWithNoneExpected]');
+    EXEC ('DROP PROCEDURE [Test_ExpectException].[Test_GoodSelectWithSomeExpected]');
+    EXEC ('DROP PROCEDURE [Test_ExpectNoException].[Test_FailSelect]');
+    EXEC ('DROP PROCEDURE [Test_ExpectNoException].[Test_FailSelectWithMessage]');
+    EXEC ('DROP PROCEDURE [Test_ExpectNoException].[Test_GoodAndFailSelectButFirstFail]');
+    EXEC ('DROP PROCEDURE [Test_ExpectNoException].[Test_GoodAndFailSelectButSecondFail]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_Can_Fake_Assembly_CLR_ScalarFunction]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_Can_Fake_Assembly_CLR_TableValuedFunction]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_ExternalInlineTableValuedFunction]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_ExternalMultiStatementTableValuedFunctionWithP1]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_ExternalScalarFunctionWithP1]');
+    EXEC ('DROP PROCEDURE [Test_FakeFunction].[Test_Failed_When_UnsuportedFunctionType]');
+    EXEC ('DROP PROCEDURE [Test_FakeTable].[Test_CanFakeExternalTable]');
+    EXEC ('DROP PROCEDURE [Test_FakeTable].[Test_CanPreserveNotNull]');
+    EXEC ('DROP PROCEDURE [Test_RemoveObject].[Test_ExternalTable]');
+    EXEC ('DROP PROCEDURE [Test_RemoveObject].[Test_ExternalTableNewName]');
+    EXEC ('DROP PROCEDURE [Test_RemoveObject].[Test_NewNameIsEmpty]');
+    EXEC ('DROP PROCEDURE [Test_RemoveObject].[Test_NewNameIsNotEmpty]');
+    EXEC ('DROP PROCEDURE [Test_SpyProcedure].[Test_ExternalProcedure]');
+    EXEC ('DROP PROCEDURE [Test_SpyProcedure].[Test_Procedure_Start_End_]');
 END;
 GO
 
